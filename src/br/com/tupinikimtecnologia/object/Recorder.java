f9f0ca658ddab7748101bd4c257da6f1e908578f
Felipe Rodrigues <felipe@gmail.com>
2016-06-01 02:03:18 -0300

    Set MainForm

--- src/br/com/tupinikimtecnologia/object/Recorder.java
+++ src/br/com/tupinikimtecnologia/object/Recorder.java
@@ -5,9 +5,15 @@
 package br.com.tupinikimtecnologia.object;
 
 import java.io.File;
+import java.io.IOException;
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -20,17 +26,54 @@ public class Recorder extends Thread {
     private AudioFileFormat.Type fileType;
     private File audioFile;
     private TargetDataLine line;
-    private AudioFormat audioFormat;
+    private final AudioFormat audioFormat;
+    private boolean isRecording;
     
     public Recorder(AudioFileFormat.Type fileType, File audioFile){
-        
-        audioFormat = new AudioFormat(16000, 8, 2, true, true);
+        this.fileType = fileType;
+        this.audioFile = audioFile;
+        this.audioFormat = new AudioFormat(16000, 8, 2, true, true);
     }
     
+    private void startRecord(){
+        try {
+
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, audioFormat);
+ 
+            line = (TargetDataLine) AudioSystem.getLine(info);
+            line.open(audioFormat);
+            line.start();   // start capturing
+ 
+            System.out.println("Start capturing...");
+ 
+            AudioInputStream ais = new AudioInputStream(line);
+ 
+            System.out.println("Start recording...");
+ 
+            // start recording
+            AudioSystem.write(ais, fileType, audioFile);
+ 
+        } catch (LineUnavailableException ex) {
+            JOptionPane.showConfirmDialog(null, "Line unavailable, check your mic.", "ERROR", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+    
+    private void stopRecord(){
+        line.stop();
+        line.close();
+        System.out.println("Finished");
+    }
 
     @Override
     public void run() {
         super.run();
+        while(isRecording){
+            if(!isRecording){
+                stopRecord();
+            }
+        }
     }
     
 }
